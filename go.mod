58abcb88fd4b95f45445efe5f16f40e27c16e517
Marcus Lee <marcustutorial@hotmail.com>
2021-04-25 13:47:10 +0800

    Update go-app to V8

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/marcustut/go-pwa
 
 go 1.15
 
-require (
-	github.com/google/uuid v1.2.0 // indirect
-	github.com/maxence-charriere/go-app/v7 v7.2.0
-)
+require github.com/maxence-charriere/go-app/v8 v8.0.2
--- main.go
+++ main.go
@@ -1,30 +1,24 @@
-// +build !wasm
-//
-// The server is a classic Go program that can run on various architecture but
-// not on WebAssembly. Therefore, the build instruction above is to exclude the
-// code below from being built on the wasm architecture.
-
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 
-	"github.com/maxence-charriere/go-app/v7/pkg/app"
+	"github.com/maxence-charriere/go-app/v8/pkg/app"
 )
 
-// The main function is the entry of the server. It is where the HTTP handler
-// that serves the UI is defined and where the server is started.
-//
-// Note that because main.go and app.go are built for different architectures,
-// this main() function is not in conflict with the one in
-// app.go.
+type hello struct {
+	app.Compo
+}
+
+func (h *hello) Render() app.UI {
+	return app.H1().Text("Hello World!")
+}
+
 func main() {
-	// app.Handler is a standard HTTP handler that serves the UI and its
-	// resources to make it work in a web browser.
-	//
-	// It implements the http.Handler interface so it can seamlessly be used
-	// with the Go HTTP standard library.
+	app.Route("/", &hello{})
+	app.RunWhenOnBrowser()
+
 	http.Handle("/", &app.Handler{
 		Name:        "Hello",
 		Description: "An Hello World! example",
