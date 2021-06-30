63430783da35b3b01857acfab654faddd60832f1
Marcus Lee <marcustutorial@hotmail.com>
2021-07-01 03:48:59 +0800

    Added docs/ folder for deployment

---
+++ docs/app-worker.js
@@ -0,0 +1,46 @@
+const cacheName = "app-" + "d53826e21d26a15c16ed208bd201919d1bf1c8de";
+
+self.addEventListener("install", event => {
+  console.log("installing app worker d53826e21d26a15c16ed208bd201919d1bf1c8de");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        "/",
+        "/app.css",
+        "/app.js",
+        "/manifest.webmanifest",
+        "/wasm_exec.js",
+        "/web/app.wasm",
+        "/web/tailwind.css",
+        "https://storage.googleapis.com/murlok-github/icon-192.png",
+        "https://storage.googleapis.com/murlok-github/icon-512.png",
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  console.log("app worker d53826e21d26a15c16ed208bd201919d1bf1c8de is activated");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
