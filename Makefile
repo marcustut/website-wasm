69599877fa592a870cc52d57302fb9c2336bdd65
Marcus Lee <marcustutorial@hotmail.com>
2021-07-01 03:47:22 +0800

    Added generateStaticPages for GitHub Pages

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-APP_NAME = go-pwa
+APP_NAME = website-wasm
 
 build:
 	GOARCH=wasm GOOS=js go build -o web/app.wasm
-	go build
+	go build -o $(APP_NAME)
 
 run: build
-	./$(APP_NAME)
+	./$(APP_NAME) local
+
+build-github: build
+	./$(APP_NAME) github
 
 tailwind:
 	npx tailwindcss -i ./web/styles.css -o ./web/tailwind.css -w
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"syscall"
 
 	"github.com/maxence-charriere/go-app/v8/pkg/app"
+	"github.com/maxence-charriere/go-app/v8/pkg/cli"
+	"github.com/maxence-charriere/go-app/v8/pkg/errors"
 	"github.com/maxence-charriere/go-app/v8/pkg/logs"
 )
 
@@ -12,11 +17,23 @@ type options struct {
 	Port int `env:"PORT" help:"The port used to run the server."`
 }
 
+type githubOptions struct {
+  Output string `cli:"o" env:"-" help:"The directory where static resources are saved."`
+}
+
 func main() {
 	app.Route("/", &App{})
 	app.RunWhenOnBrowser()
 
-	http.Handle("/", &app.Handler{
+  ctx, cancel := cli.ContextWithSignals(
+    context.Background(),
+    os.Interrupt,
+    syscall.SIGTERM,
+  )
+  defer cancel()
+  defer exit()
+
+  h := app.Handler{
 		Author:          "Marcus Lee",
 		BackgroundColor: "#24283b",
 		ThemeColor:      "#24283b",
@@ -27,14 +44,55 @@ func main() {
 		Styles: []string{
 			"/web/tailwind.css",
 		},
-	})
+	}
 
 	opts := options{Port: 8001}
+  cli.Register("local").
+    Help("Launches a server that serves the app in a local environment.").
+    Options(&opts)
 
-	app.Logf("%s", logs.New("Starting server").Tag("port", opts.Port))
+  githubOpts := githubOptions{Output: "docs"}
+  cli.Register("github").
+    Help("Generates the required resources to run app on GitHub Pages.").
+    Options(&githubOpts)
+  cli.Load()
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%v", opts.Port), nil); err != nil {
-		panic(err)
-	}
+  switch cli.Load() {
+  case "local":
+    runLocal(ctx, &h, opts)
+  case "github":
+    generateGitHubPages(ctx, &h, githubOpts)
+  }
 }
 
+func runLocal(ctx context.Context, h http.Handler, opts options) {
+  app.Logf("%s", logs.New("starting app server").Tag("port", opts.Port))
+
+  s := http.Server{
+    Addr: fmt.Sprintf(":%v", opts.Port),
+    Handler: h,
+  }
+
+  go func() {
+    <-ctx.Done()
+    s.Shutdown(context.Background())
+  }()
+
+  if err := s.ListenAndServe(); err != nil {
+    panic(err)
+  }
+}
+
+func generateGitHubPages(ctx context.Context, h *app.Handler, opts githubOptions) {
+  if err := app.GenerateStaticWebsite(opts.Output, h); err != nil {
+    panic(err)
+  }
+}
+
+func exit() {
+  err := recover()
+  if err != nil {
+    app.Logf("command failed: %s", errors.Newf("%v", err))
+    os.Exit(-1)
+  }
+}
